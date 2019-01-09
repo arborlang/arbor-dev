e9bec1238915546df786b75cbc584572b5899f68
Yoseph Radding <yradding@redventures.com>
2019-01-09 17:27:01 -0500

    Initial Commit

---
+++ Extension.go
@@ -0,0 +1,55 @@
+package arbor
+
+import (
+	"github.com/perlin-network/life/exec"
+)
+
+//VirtualMachine is just so that we can pull in arbor and nothing else
+type VirtualMachine exec.VirtualMachine
+
+// Extension is an Extension that runs in go (or via cgo). You can use this to define native behavior
+type Extension interface {
+	Run(*exec.VirtualMachine) int64
+}
+
+// ExtensionFunc are simple functions that implements a module behaviour
+type ExtensionFunc func(vm *exec.VirtualMachine) int64
+
+// Run implements the resolver
+func (r ExtensionFunc) Run(vm *exec.VirtualMachine) int64 {
+	return r(vm)
+}
+
+// Module defines an API to add native go modules (or in c through cgo) to Arbor
+// As long as the module is in the path, this should load fine
+type Module interface {
+	// Resolve attempts to find a Resolver to call that corresponds to the name
+	Resolve(string) Extension
+	// Name gets the name of the module
+	Name() string
+}
+
+// Resolver is an implementation of the Module interface
+type Resolver struct {
+	ModuleName string
+	Execers    map[string]Extension
+}
+
+// Register registers an extension
+func (r *Resolver) Register(name string, e Extension) {
+	r.Execers[name] = e
+}
+
+// Resolve finds the extenstion that was registeredcd
+func (r *Resolver) Resolve(name string) Extension {
+	e, ok := r.Execers[name]
+	if !ok {
+		return nil
+	}
+	return e
+}
+
+// Name get the module name
+func (r *Resolver) Name() string {
+	return r.ModuleName
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/radding/arbor-dev
+
+replace github.com/go-interpreter/wagon v0.0.0 => github.com/perlin-network/wagon v0.3.1-0.20180825141017-f8cb99b55a39
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/perlin-network/life v0.0.0-20181215083421-28a99a6d79ec
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
+	github.com/urfave/cli v1.20.0 // indirect
+)
